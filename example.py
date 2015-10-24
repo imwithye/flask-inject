e96b2a184e7a4b241ea7989857b04ca700a70f00
Ciel <imwithye@gmail.com>
2015-10-24 16:22:17 +0800

    add documentation to the example code

--- example.py
+++ example.py
@@ -1,58 +1,98 @@
-from flask import Flask
+from flask import Flask, request
 from flask_inject import Inject, inject
 from functools import wraps
 import logging
 
 app = Flask(__name__)
-Inject(app)
+
+# Add Inject framework to the Flask App
+# It will setup the all necessary handlers
+# The method will also return a global injector which you can call map, get and apply on it
+inj = Inject(app)
+
+# When you call map on this global injector, it will attach the value to the corresponding key.
+# Note that this is a global or shared injector, so the variables are shared cross all handlers.
+# If you want have a injector for each different request, setup injector in before_request handler.
+inj.map(version="v1.0")
+
+# get method will return the value of a key. This is a reclusive lookup strategy. That means if the key
+# presents in the injector, it will simply return the value. But if the key does not present, it will
+# lookup this key in its parent injector
+
+# This global injector has no parent. If "version" is not a valid key, it simply returns None. But global
+# injector is the parent injector of per request injector, so the value inside global injector is available
+# to each per request injector.
+# Again, the value in global injector is shared cross all handlers.
+v = inj.get("version")
 
 
+# Set up per request injector.
+# Here we try to open a mysql connection for each request and in tear down method, we will close this connection
+# Using @inject decorator to inject the dependencies to the handler
 @app.before_request
 @inject("injector")
 def before_request(injector):
-    version = "v1.0"
-    mysql = "mysql connection"
-    injector.map(version=version, mysql=mysql)
+    # Open mysql connection here
+    mysql = "open mysql here"
+    # Add mysql to the injector so that it will be available for handlers after before request
+    injector.map(mysql=mysql)
 
 
+# Now we want to close the mysql connection
+# Using @inject decorator to inject the mysql dependency to the handler
 @app.teardown_request
 @inject("mysql")
 def teardown_request(exception, mysql):
-    logging.info("teardown request")
-    logging.info(mysql)
+    # close mysql here
+    mysql = "close mysql here"
 
 
-def authentication():
-    def decorator(f):
-        @inject("injector")
-        @wraps(f)
-        def decorated_function(injector, *args, **kwargs):
-            injector.map(auth=True)
-            return injector.apply(["auth"], f, args, kwargs)
-
-        return decorated_function
-
-    return decorator
-
-
+# Suppose we need show the app version in this handler. We add the version to the global injector and as we
+# mentioned before, the global injector is the parent injector of each per request injector. So here the version
+# is also available.
+# Using @inject decorator to inject the dependencies to the handler
 @app.route("/version")
 @inject("version")
 def show_version(version):
     return version
 
 
+# Suppose we need access mysql database here
+# Since we add mysql to the injector before request
+# So just use @inject decorator to inject the dependencies to the handler
 @app.route("/mysql")
 @inject("mysql")
 def show_mysql(mysql):
-    if mysql:
-        return "success"
-    return "fail"
+    # do something with mysql
+    mysql = "exec a query"
+    return "success"
 
 
+# Injector is also available in the function decorator
+# Suppose we have an authentication decorator which checks the auth_token parameter in the url parameters.
+def authentication(f):
+    @inject("injector")
+    @wraps(f)
+    def decorated_function(injector, *args, **kwargs):
+        # Check the aut_token here
+        auth_token = request.args.get("auth_token")
+        authorized = False
+        if auth_token is not None and len(auth_token) > 0:
+            authorized = True
+        # Now we can map the result to the injector
+        injector.map(authorized=authorized)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# Then authorized is available for injecting to the handler
+# Using @inject decorator to inject the dependencies to the handler
 @app.route("/auth")
-@authentication()
-def show_auth(auth):
-    if auth:
+@authentication
+@inject("authorized")
+def show_auth(authorized):
+    if authorized:
         return "200"
     return "401"
 
--- flask_inject.py
+++ flask_inject.py
@@ -4,37 +4,54 @@ from functools import wraps
 
 class Inject(object):
     class Injector(object):
-        def __init__(self):
+        def __init__(self, parent=None):
+            if parent is not None:
+                assert isinstance(parent, Inject.Injector)
+            self.parent = parent
             self._base = dict()
 
         def map(self, *args, **kwargs):
             for k, v in kwargs.iteritems():
-                self.map_to(k, v)
+                assert isinstance(k, str)
+                self._base[k] = v
+            return self
 
-        def map_to(self, key, value):
-            assert isinstance(key, str)
-            self._base[key] = value
-
-        def lookup(self, key):
-            return self._base.get(key)
+        def get(self, key):
+            value = self._base.get(key)
+            if value is None:
+                if self.parent is not None:
+                    return self.parent.get(key)
+                return None
+            return value
 
         def apply(self, keys, func, args, kwargs):
             for key in keys:
-                kwargs[key] = self.lookup(key)
+                kwargs[key] = self.get(key)
             return func(*args, **kwargs)
 
     def __init__(self, app):
-        self.app = app
+        self._injector = self.Injector()
+        self._app = app
         if app is not None:
             self._init_app(app)
 
     def _init_app(self, app):
-        app.before_request(self.before_request)
+        app.before_request(self._before_request)
 
-    def before_request(self):
-        _injector = self.Injector()
+    def _before_request(self):
+        _injector = self.Injector(self._injector)
         _injector.map(injector=_injector)
         g._injector = _injector
+
+    def map(self, *args, **kwargs):
+        self._injector.map(*args, **kwargs)
+        return self
+
+    def get(self, key):
+        return self._injector.get(key)
+
+    def apply(self, keys, func, args, kwargs):
+        return self._injector.apply(keys, func, args, kwargs)
 
 
 def inject(*keys):
